58a6285bf256eae46118bc64db808dd16083c3df
IGOR2 <i.kryvopustov@andersenlab.com>
2016-05-10 11:02:52 +0300

    Add filter and search

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -2,13 +2,14 @@ var appModuleControllers = angular.module('appModuleControllers', []);
 
 appModuleControllers.controller('mainCtrl', ['$scope','$http', 
 	function($scope, $http){
-		$http.get('http://api.openweathermap.org/data/2.5/find?lat=50.4&lon=30.5&cnt=50&APPID=58f420f86326565c7d552af951c62bbb').success(function(data){
+		$http.get('http://api.openweathermap.org/data/2.5/box/city?bbox=30.0,48.0,33.0,51.0,1111&cluster=yes&APPID=58f420f86326565c7d552af951c62bbb').success(function(data){
 			$scope.button = data;
 	})
 
 	$scope.myClick = function(id){
 		$http.get('http://api.openweathermap.org/data/2.5/forecast/city?id='+id+'&APPID=58f420f86326565c7d552af951c62bbb').success(function(data){
 			$scope.allInfo = data;
+      console.log(data);
 		});	
 	};
 
@@ -16,4 +17,7 @@ appModuleControllers.controller('mainCtrl', ['$scope','$http',
     var celsius = temp - 273.15;
     return celsius.toFixed(2);
 	};
+
+  $scope.sortBy = 'name';
+  
 }])
